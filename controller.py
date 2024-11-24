b95a4785d20de88c30aadd4bc6761487170b3a66
Jaime de Roque <jaimedrq@gmail.com>
2024-11-24 21:32:42 +0100

    stable version

--- controller.py
+++ controller.py
@@ -10,10 +10,7 @@ if __name__ == '__main__':
     """Servicio para descargar y comprimir archivos."""
     try:
       url = request.args.get('url', None)
-      if not url:
-        return jsonify({'error': 'URL no proporcionada'}), 400
-      if not url.lower().endswith(('.exe', '.msi')):
-        return jsonify({'error': 'URL debe ser un archivo .exe o .msi'}), 400
+      if not url: return jsonify({'error': 'URL no proporcionada'}), 400
 
       downloaded_file, filename = download_file(url)  # Descargar el archivo
       
@@ -28,8 +25,14 @@ if __name__ == '__main__':
       # Configurar una función de callback para eliminar el archivo después de enviarlo
       @response.call_on_close
       def cleanup():
+        """"""
+        print("wasabi!")
+        print(f"Intentando eliminar el archivo: {downloaded_file}")
         if os.path.exists(downloaded_file):
           os.remove(downloaded_file)
+          print(f"Archivo eliminado: {downloaded_file}")
+        else:
+          print(f"El archivo no existe: {downloaded_file}")
       
       return response
 
@@ -41,3 +44,8 @@ if __name__ == '__main__':
   # RUN
   # ----------------
   app.run(debug=True, host='0.0.0.0', port=80)
+
+  # ----------------
+  # CALL FUNCTION
+  # ----------------
+  # curl -O -J "http://localhost/downloadzipper?url=https://image.freepik.com/vector-gratis/ilustracion-vector-prueba-trabajo-examen_138676-243.jpg"
